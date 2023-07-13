794b2b5d8093460b5bf79562ee1227bc0b12a7ba
Fernando Lopez <itsfercodes@outlook.com>
2023-07-13 14:18:31 -0600

    Add basic counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'basics';
+  // Properties
+  public title: string = 'My first Angular app';
+  public counter: number = 0;
+
+  increaseBy(value: number): void {
+    this.counter += value;
+  }
+
+  resetCounter(): void {
+    this.counter = 0;
+  }
 }
