72b166c9e00547408dff7d3ec4f27e1dad041557
Fernando Lopez <itsfercodes@outlook.com>
2023-07-21 20:25:27 -0600

    Add dbz module and pass properties from parent to child component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CounterModule } from './counter/counter.module';
-import { HeroesModule } from './heroes/hero.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CounterModule, HeroesModule],
+  imports: [BrowserModule, DbzModule],
   providers: [],
   bootstrap: [AppComponent],
 })
